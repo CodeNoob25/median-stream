c3cf298a590ff36f9a5835315f56450d3bb3178a
justinposer <jposer1@gmail.com>
2019-02-05 14:21:24 -0500

    finished basic binary heap

--- heap-practice.js
+++ heap-practice.js
@@ -4,8 +4,11 @@ class Heap {
         this.storage = [];
     }
 
+    returnRoot() {
+        return this.storage[0];
+    }
 
-    insert(value) {
+    push(value) {
         this.storage.push(value);
         var valueIndex = this.storage.length - 1;
         var parentIndex = Math.floor(this.storage.length / 2);
@@ -17,6 +20,34 @@ class Heap {
             parentIndex = Math.floor(valueIndex / 2);
         }
     }
+
+    pop() {
+        var removed = this.storage[0];
+
+        this.storage[0] = this.storage[this.storage.length - 1];
+        this.storage.pop();
+        if (this.storage.length < 1) {
+            return null;
+        }
+        var changeIndex = 0;
+        var childIndex1 = 1;
+        var childIndex2 = 2;
+        var swapIndex, temp;
+        while ((compareFunction(this.storage[childIndex1], this.storage[changeIndex]) > 0) || (compareFunction(this.storage[childIndex2], this.storage[changeIndex]) > 0)) {
+            if ((compareFunction(this.storage[childIndex1], this.storage[childIndex2]) > 0)  || !this.storage[childIndex2]) {
+                swapIndex = childIndex1;
+            } else {
+                swapIndex = childIndex2;
+            }
+            temp = this.storage[swapIndex];
+            this.storage[swapIndex] = this.storage[changeIndex];
+            this.storage[changeIndex] = temp;
+            changeIndex = swapIndex;
+            childIndex1 = (changeIndex * 2) + 1;
+            childIndex2 = (changeIndex * 2) + 2;
+        }
+        return removed;
+    }
 }
 
 var compareFunction = compareFunction || function(a, b) {return b - a};
@@ -24,8 +55,14 @@ var compareFunction = compareFunction || function(a, b) {return b - a};
 
 var testHeap = new Heap();
 
-for (var i = 0; i < 10; i++) {
-    testHeap.insert(Math.floor(Math.random() * 999))
+for (var i = 0; i < 50; i++) {
+    testHeap.push(Math.floor(Math.random() * 999))
 }
 
 console.log(testHeap.storage)
+
+for (var i = 0; i < 50; i++) {
+    testHeap.pop()
+    console.log(testHeap.storage)
+}
+
